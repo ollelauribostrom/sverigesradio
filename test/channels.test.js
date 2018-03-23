115ae41eef05d14677cb02d5f3a47a94dd6e8e10
Olle Lauri Boström <ollebostr@gmail.com>
2018-03-23 09:38:20 +0100

    Moving tests to jest

--- test/channels.test.js
+++ test/channels.test.js
@@ -1,19 +1,21 @@
-import chai, { expect } from 'chai';
 import channels from '../src/channels';
 
 describe('channels.js', () => {
   it('module should expose the correct properties', () => {
-    expect(channels).to.have.all.keys(['P1', 'P2', 'P3', 'active', 'select']);
+    expect(channels).toHaveProperty('P1');
+    expect(channels).toHaveProperty('P2');
+    expect(channels).toHaveProperty('P3');
+    expect(channels).toHaveProperty('select');
   });
   it('active channel should default to P3', () => {
-    expect(channels.active.name).to.equal('P3');
+    expect(channels.active.name).toEqual('P3');
   });
   it('select should default to P3', () => {
     channels.select('Some nonexisting channel');
-    expect(channels.active.name).to.equal('P3');
+    expect(channels.active.name).toEqual('P3');
   });
   it('select should update the active channel', () => {
     channels.select('p1');
-    expect(channels.active.name).to.equal('P1');
+    expect(channels.active.name).toEqual('P1');
   });
 });
