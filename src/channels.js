379ec3e7b754d4b460f48ea1181b7ff9143c8743
Olle Lauri Boström <ollebostr@gmail.com>
2018-09-04 21:31:49 +0200

    Use https

--- src/channels.js
+++ src/channels.js
@@ -1,15 +1,15 @@
 const channels = {
   P1: {
     name: 'P1',
-    url: 'http://sverigesradio.se/topsy/direkt/srapi/132.mp3',
+    url: 'https://sverigesradio.se/topsy/direkt/srapi/132.mp3',
   },
   P2: {
     name: 'P2',
-    url: 'http://sverigesradio.se/topsy/direkt/srapi/163.mp3',
+    url: 'https://sverigesradio.se/topsy/direkt/srapi/163.mp3',
   },
   P3: {
     name: 'P3',
-    url: 'http://sverigesradio.se/topsy/direkt/srapi/164.mp3',
+    url: 'https://sverigesradio.se/topsy/direkt/srapi/164.mp3',
   },
 };
 
